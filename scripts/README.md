84bc4824d8be49de1c637da71a6d0aefe5b21126
asqwerty666 <asqwerty@gmail.com>
2022-01-15 12:46:06 +0100

    first example added

---
+++ scripts/README.md
@@ -0,0 +1,13 @@
+# example scripts
+
+Here you can find some scripts that show how to use the helper
+
+### slurm\_list.py
+
+The script reads a file with a list of orders and executes them with the same priority
+
+```
+$ ./slurm_list.py my_orders.list
+```
+
+
---
+++ scripts/slurm_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+ 
+import sys
+import os
+from slurm import send_sbatch 
+
+jtime = '3:0:0'
+cpus = 4
+
+ifile = str(sys.argv[1])
+wdir = 'slurm'
+if not os.path.isdir(wdir): os.mkdir(wdir)
+count = 0
+ljob = {'job_name':ifile, 'cpus':cpus, 'mem_per_cpu':'4G', 'time':jtime, 'output':wdir+'/'+ifile+'order-%j', 'mailtype':'FAIL,TIME_LIMIT,STAGE_OUT'}
+with open(ifile, 'r') as orf:
+  for line in orf:
+    count+=1
+    ljob['filename'] = wdir+'/sorder_{:04d}'.format(count)+'.sh'
+    send_sbatch(ljob)
+ejob = {'job_name':ifile, 'output':wdir+'/'+ifile+'end-%j', 'filename':wdir+'/sorder_end.sh'}
+send_sbatch(ejob)
